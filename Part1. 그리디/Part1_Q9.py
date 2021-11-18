35405c64e9e30a0f976ccff9d98913d190b04d7b
olo90632951 <olo90632951@gmail.com>
2021-11-18 11:18:56 +0900

    2019 카카오 LV4 무지의 먹방 라이브 (프로그래머스)

--- Part1. 그리디/Part1_Q9.py
+++ Part1. 그리디/Part1_Q9.py
@@ -1,10 +1,10 @@
 #Part1. 그리디
 #문제 9. 2019 카카오 LV4 무지의 먹방 라이브 (프로그래머스)
+# [3,1,2] 가 있을 때 n초 후에 나타내는 자리는? 1초당 한번씩 옆으로 이동하며, 0초일 때는 pass 
 
 import heapq
 
 def solution(food_times,k):
-    
     if sum(food_times) <= k:    #나중에 모두 0이 되는데 k가 남아있는데 갈 곳이 없을 때
         return -1
 
@@ -21,7 +21,6 @@ def solution(food_times,k):
 
     #최소값 구한 후 그 값이 0이 될 때까지 몇바퀴 돌아야되는지만 구하는 것  
     #q[0][0] - previous는 다음 최솟값에도 이전 최솟값만큼 1바퀴당 -1씩 해줘야되니깐
-
     while sum_value + (( q[0][0] - previous ) * length) <= k:   #몇바퀴 도는지만 구하기 위해 (나머지만 있을 때는 사용 안하는 조건)
         now = heapq.heappop(q)[0]     #최소값 지우기(루트 노트)   // # now는 최솟값 나타냄
         sum_value += (now - previous) * length   #1바퀴당 몇번 계산하는지 구함
