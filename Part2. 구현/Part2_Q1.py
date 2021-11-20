956627f86c521c4f4435f6c526a879fd0fcc6387
olo90632951 <olo90632951@gmail.com>
2021-11-20 14:08:40 +0900

    (구현/시물레이션) (1,1)에서 시작해서 동서남북으로 이동해서 마지막 구하기

--- Part2. 구현/Part2_Q1.py
+++ Part2. 구현/Part2_Q1.py
@@ -1,32 +1,32 @@
 #Part2. 구현
-#문제 1. (1,1) 에서 시작해서 입력받은(L,R,U,D)중에서 받은 대로 1칸씩 이동해서 마지막 인덱스 출력하기
+#문제 1. (1,1) 에서 시작해서 입력받은(L,R,U,D)중에서 받은 대로 1칸씩 이동해서 마지막 인덱스 출력하기   (맨 왼쪽 위에서 시작)
+# 동서남북 한칸씩 이동 가능   
+# 칸의 크기 밖에 이동 불가
   
 n = int(input())
-list_a=list(input().split())
+
+x = 1
+y = 1   #시작점 설정해주기
+
+plans = input().split()
+
+dx = [0,0,-1,1]   #동서남북 한칸 설정
+dy = [-1,1,0,0]
+
+move_types = ['L','R','U','D']
+
+for plan in plans:
+  for i in range(len(move_types)):
+    if plan == move_types[i]:  #중요!
+      nx = x + dx[i]
+      ny = y + dy[i]    #이동 좌표 구하기
   
-a = [1,1]
+  #공간 밖에 있는 경우 무시하기 
+  if nx < 1 or ny < 1 or nx > n or ny > n:
+    continue
 
-for i in list_a:
-  if i == 'R':
-    if a[1] + 1 == 0 or a[1] + 1 > n :
-      continue
-    else:
-      a[1] += 1
-  elif i == 'U':
-    if a[0] - 1 == 0 or a[0] - 1 > n :
-      continue
-    else:
-      a[0] -= 1
-  elif i == 'D':
-    if a[0] + 1 == 0 or a[0] + 1 > n :
-      continue
-    else:
-      a[0] += 1
-  else:
-    if a[1] - 1 == 0 or a[1] - 1 > n:
-      continue
-    else:
-      a[1] -= 1
+  #공간 안에 있으면 x,y에 값 넣기   
+  x = nx
+  y = ny
 
-for i in a: 
-  print(i,end=" ")
+print(x,y)
